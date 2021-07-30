6c5b282933a19383c0d73b98263458ef1bc00bf2
Manas Maheswari <62172748+manasxd17@users.noreply.github.com>
2021-07-31 01:50:52 +0530

    Delete operators directory

--- operators/operators1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def area(r):
-    return (math.pi)*r**2
-
-print(area(float(input())))
--- operators/operators3.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-
-def nDays(d1,d2):
-    x = d2-d1
-    return x.days
-    
-print(nDays(date(2014,7,2),date(2014,7,11)))
