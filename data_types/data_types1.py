8dec3ec1beae45f231c99bbae275b4592bf82746
Manas Maheswari <62172748+manasxd17@users.noreply.github.com>
2021-07-31 01:49:32 +0530

    Delete data_types directory

--- data_types/data_types1.py
+++
@@ -1,7 +0,0 @@
-def func(a):
-    if(len(a) >= 2):
-        return a[:2] + a[-2:]
-    else:
-        return f"Empty String"
-
-print(func(input()))
--- data_types/data_types2.py
+++
@@ -1,8 +0,0 @@
-def maxFunc(mylist:list):
-    max = mylist[0]
-    for i in mylist:
-        if i > max:
-            max=i
-    return max
-
-print(maxFunc(input().split()))
