274a5ac067f4f879d5bca0e8151f12d8c746ee06
Manas Maheswari <62172748+manasxd17@users.noreply.github.com>
2021-07-31 01:50:15 +0530

    Delete loops directory

--- loops/loop1.py
+++
@@ -1,9 +0,0 @@
-def conCat(mylist:list):
-    result = ""
-    for words in mylist:
-        words = str(words)
-        result += words
-    return result
-
-print(conCat(["Hi","Bye",17]))
-    
--- loops/loop2.py
+++
@@ -1,4 +0,0 @@
-def func(s1,s2):
-    return s1.difference(s2)
-
-print(func(set(["White","Black","Red"]),set(["Red","Green"])))
