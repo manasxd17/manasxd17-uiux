11557f155c9854821368588502eae7cb21da0357
Manas Maheswari <62172748+manasxd17@users.noreply.github.com>
2021-07-31 01:50:29 +0530

    Delete modules_section directory

--- modules_section/import_here.py
+++
@@ -1,7 +0,0 @@
-from city import details
-
-mydict = {'Names':'Manas','City':'Kota','Population':2100000,'Pincode':324006}
-m = details(mydict)
-print(m.getpin())
-print(m.getPopulatn())
-print(m.getCity())
--- modules_section/sinfunc.py
+++
@@ -1,4 +0,0 @@
-from math import sin
-
-a = sin(1)
-print(a)
